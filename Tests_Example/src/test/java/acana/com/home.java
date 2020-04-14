fc59b188d1259aa42abeaf7646971d0d00c66086
Amdrei Kalianchuk <andrey.kalenchuk@gmail.com>
2020-04-14 17:19:48 -0400

    asana.com added pacage

---
+++ Tests_Example/src/test/java/acana/com/home.java
@@ -0,0 +1,12 @@
+package acana.com;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class home {
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+}
---
+++ Tests_Example/src/test/java/acana/com/homePage.java
@@ -0,0 +1,48 @@
+package acana.com;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class homePage {
+	private static WebDriver driver;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		WebDriverManager.chromedriver().setup();
+	}
+	
+	@BeforeEach
+	void setUp() {
+		driver = new ChromeDriver();
+//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://asana.com/");
+	}
+
+	@AfterEach
+	void tearDown() {
+		driver.quit();
+	}
+
+	@Test
+	void test(){
+		driver.findElement(By.id("navigation__dropdown-toggle-why-asana")).click();
+		
+		WebElement dropDown = new WebDriverWait(driver, 10)
+				.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".navigation__dropdown.-active")));
+		assertTrue(dropDown.isDisplayed());
+	}
+}
+// Thread.sleep(2000);
